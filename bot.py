4d648e13d9d10c825560b7c91542c330c597d725
Sebastian Repko <115408696+SebaRepko@users.noreply.github.com>
2023-01-10 12:01:35 +0100

    amendments
    amendments

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from discord import app_commands
 from discord.ext import commands
 from discord.ui import Button, View
 from csgoUser import csgoUser
+from apex import apexstats
 from tarkov import get_item_data
 from tarkov import get_tier
 from datetime import datetime
@@ -140,42 +141,73 @@ class regions(Enum):
 @tree.command(name="summonerstats", description="See your stats in League of Legends")
 async def command(interaction: discord.Interaction, nickname: str, region: regions):
     try:
+        app_commands.choices
         player = summonerstats(nickname, region.value) 
         view = View()
         embed = discord.Embed(title=f"Nick: {player.username}", color=0x00bfff)
         embed.set_thumbnail(url=player.icon)
+        ts1 = int(f'{player.championMasteryLastplaytime1}')
+        ts2 = int(f'{player.championMasteryLastplaytime2}')
+        ts3 = int(f'{player.championMasteryLastplaytime3}')
+        ts1 /= 1000
+        ts2 /= 1000
+        ts3 /= 1000
+        date1 = datetime.utcfromtimestamp(ts1).strftime('%d-%m-%Y')
+        date2 = datetime.utcfromtimestamp(ts2).strftime('%d-%m-%Y')
+        date3 = datetime.utcfromtimestamp(ts3).strftime('%d-%m-%Y')
         embed.add_field(name="Summoner level:", value=f"{player.summonerLevel}", inline=False)
-        if hasattr(player,"tier") != 0:
+        if hasattr(player,'tier') != 0:
             embed.add_field(name="Rank Solo/Duo:", value=f"{player.tier} {player.rank}\n\n **Wins:** {player.wins}\n **Losses:** {player.losses}\n **All Games:** {player.allGames}\n **Winratio:** {round(player.winratio,2)}%",inline=True)
-        if hasattr(player,"tierFlex") != 0:
+        if hasattr(player,'tierFlex') != 0:
             embed.add_field(name="Rank Flex:", value=f"{player.tierFlex} {player.rankFlex}\n\n **Wins:** {player.winsFlex}\n **Losses:** {player.lossesFlex}\n **All Games:** {player.allGamesFlex}\n **Winratio:** {round(player.winratioFlex,2)}%",inline=True)
-        if hasattr(player,"championMasteryName1") != 0:
-            ts1 = int(f'{player.championMasteryLastplaytime1}')
-            ts2 = int(f'{player.championMasteryLastplaytime2}')
-            ts3 = int(f'{player.championMasteryLastplaytime3}')
-            ts1 /= 1000
-            ts2 /= 1000
-            ts3 /= 1000
-            date1 = datetime.utcfromtimestamp(ts1).strftime('%d-%m-%Y')
-            date2 = datetime.utcfromtimestamp(ts2).strftime('%d-%m-%Y')
-            date3 = datetime.utcfromtimestamp(ts3).strftime('%d-%m-%Y')
-            embed.add_field(name="Total mastery points:", value=f"{player.totalChampionMastery}", inline=False)
-            embed.add_field(name="***Top 3 champions by mastery points:***", value="\u200b", inline=False)
-            embed.add_field(name=f"**Top 1**", value=f"> **Name:**\n>  {player.championMasteryName1}\n> **Lvl:** {player.championMasteryLevel1}\n> **Mastery Points:**\n> {format(player.championMasteryPoints1,',')}\n> **Last played in:** {date1}",inline=True)
-            embed.add_field(name=f"**Top 2**", value=f"> **Name:**\n> {player.championMasteryName2}\n> **Lvl:** {player.championMasteryLevel2}\n> **Mastery Points:**\n> {format(player.championMasteryPoints2,',')}\n> **Last played in:** {date2}",inline=True)
-            embed.add_field(name=f"**Top 3**", value=f"> **Name:**\n> {player.championMasteryName3}\n> **Lvl:** {player.championMasteryLevel3}\n> **Mastery Points:**\n> {format(player.championMasteryPoints3,',')}\n> **Last played in:** {date3}",inline=True)
-        embed.add_field(name="Data povided by:", value = "https://developer.riotgames.com/", inline=False)
+        embed.add_field(name="Total mastery points:", value=f"{player.totalChampionMastery}", inline=False)
+        embed.add_field(name="***Top 3 champions by mastery points:***", value="\u200b", inline=False)
+        embed.add_field(name=f"**Top 1**", value=f"> **Name:**\n>  {player.championMasteryName1}\n> **Lvl:** {player.championMasteryLevel1}\n> **Mastery Points:**\n> {format(player.championMasteryPoints1,',')}\n> **Last played in:** {date1}",inline=True)
+        embed.add_field(name=f"**Top 2**", value=f"> **Name:**\n> {player.championMasteryName2}\n> **Lvl:** {player.championMasteryLevel2}\n> **Mastery Points:**\n> {format(player.championMasteryPoints2,',')}\n> **Last played in:** {date2}",inline=True)
+        embed.add_field(name=f"**Top 3**", value=f"> **Name:**\n> {player.championMasteryName3}\n> **Lvl:** {player.championMasteryLevel3}\n> **Mastery Points:**\n> {format(player.championMasteryPoints3,',')}\n> **Last played in:** {date3}",inline=True)
+        embed.add_field(name="Data povided by:", value = "https://www.leagueoflegends.com/", inline=False)
         await interaction.response.send_message(embed=embed, view=view)
     except:
         view = View()
         embed = discord.Embed(title="ERROR 404: NOT FOUND", color=0x00bfff)
         embed.add_field(name=f"User {nickname} on region {region.name} do not exist", value="Check the spelling, or try with other region", inline=False)
-        embed.set_footer(text="Data povided by: https://developer.riotgames.com/")
+        embed.set_footer(text="Data povided by: https://www.leagueoflegends.com/")
         await interaction.response.send_message(embed=embed, view=view)
-        
+
+class ApexPlatforms(Enum):
+    Origin = "origin"
+    XboxLive = "xbl"
+    PlayStationNetwork = "psn"
+
+@tree.command(name="apexstats", description="See your stats in Apex")
+async def command(interaction: discord.Interaction, nickname: str, platform: ApexPlatforms):
+    apexPlayer = apexstats(platform=platform.value, nickname=nickname)
+    print(apexPlayer.response)
+    if apexPlayer.response == 200:
+        view = View()
+        apexPlayer = apexstats(platform=platform.value, nickname=nickname)
+        embed = discord.Embed(title=f"{nickname}", color=0x00bfff)
+        embed.set_thumbnail(url=apexPlayer.avatarUrl)
+        embed.add_field(name="Kills: ", value=apexPlayer.kills, inline=True)
+        embed.add_field(name="Level: ", value=apexPlayer.level, inline=True)
+        embed.add_field(name="Damage: ", value=apexPlayer.damage, inline=True)
+        embed.add_field(name=f"**Ranked**", value=f"> **Rank:**\n>  {apexPlayer.rankName}\n> **MMR:**\n> {apexPlayer.rankValue}\n", inline=True)
+        embed.add_field(name=f"**Arena**", value=f"> **Rank:**\n>  {apexPlayer.arenaRankName}\n> **MMR:**\n> {apexPlayer.arenaRankValue}\n", inline=True)
+        embed.add_field(name=f"**Ranked peak**", value=f"> **Rank peak:**\n>  {apexPlayer.peakRankName}\n> **MMR peak:**\n> {apexPlayer.peakRankValue}\n", inline=True)
+        embed.set_footer(text="Data povided by: https://tracker.gg/apex")
+        await interaction.response.send_message(embed=embed, view=view)
+
+    else:
+        view = View()
+        embed = discord.Embed(title="ERROR 404: NOT FOUND", color=0x00bfff)
+        embed.add_field(name=f"User {nickname} on platform {platform.name} do not exist", value="Check the spelling, or try with other platform", inline=False)
+        embed.set_footer(text="Data povided by: https://tracker.gg/apex")
+        await interaction.response.send_message(embed=embed, view=view)
+
+
 @tree.command(name="tftstats", description="See your stats in Teamfight Tactics")
 async def command(interaction: discord.Interaction, nickname: str, region: regions):
-    #try:
+    try:
         player = tftstats(nickname, region.value)
         view = View()
         embed = discord.Embed(title=f"Nick: {player.tftUsername}", color=0x00bfff)
@@ -189,12 +221,12 @@ async def command(interaction: discord.Interaction, nickname: str, region: regio
             embed.add_field(name="Rank Double:", value=f"{player.tftDoubleTier} {player.tftDoubleRank}\n\n> **Wins:** {player.tftDoubleWins}\n> **Losses:** {player.tftDoubleLosses}\n> **All Games:** {player.tftDoubleAllGames}\n> **Winratio:** {round(player.tftDoubleWinratio,2)}%",inline=True)
         embed.add_field(name="Data povided by:", value = "https://developer.riotgames.com/", inline=False)
         await interaction.response.send_message(embed=embed, view=view)
-   # except:
-    #    view = View()
-     #   embed = discord.Embed(title="ERROR 404: NOT FOUND", color=0x00bfff)
-      #  embed.add_field(name=f"User {nickname} on region {region.name} do not exist", value="Check the spelling, or try with other region", inline=False)
-       # embed.set_footer(text="Data povided by: https://developer.riotgames.com/")
-       # await interaction.response.send_message(embed=embed, view=view)   
+    except:
+        view = View()
+        embed = discord.Embed(title="ERROR 404: NOT FOUND", color=0x00bfff)
+        embed.add_field(name=f"User {nickname} on region {region.name} do not exist", value="Check the spelling, or try with other region", inline=False)
+        embed.set_footer(text="Data povided by: https://developer.riotgames.com/")
+        await interaction.response.send_message(embed=embed, view=view)   
 
 
 @client.event
